4e8e2fd7e84ff55f8f3b624289688155b8d4a3ad
ateals <ateals@icloud.com>
2024-03-23 17:13:17 +0900

    : root Layout ProviderGroup 추가

--- src/widgets/ProviderGroups.tsx
+++ src/widgets/ProviderGroups.tsx
@@ -1,5 +1,5 @@
 import { NaverMapProvider } from "@/feature/Provider";
 
-export const ProviderGrups = ({ children }: { children: React.ReactNode }) => {
+export const ProviderGroups = ({ children }: { children: React.ReactNode }) => {
   return <NaverMapProvider>{children}</NaverMapProvider>;
 };
