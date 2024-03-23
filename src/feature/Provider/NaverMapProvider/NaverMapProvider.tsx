74d1c5a8643171958fb491d0275582bb1faf79b2
ateals <ateals@icloud.com>
2024-03-23 17:38:48 +0900

    fix: NaverMapProvider에서 MapContainer 분리
    MapContainer에 Suspense가 존재하기 때문에 Provider가 아닌 페이지에서 wrapping 해서 사용해야함

--- src/feature/Provider/NaverMapProvider/NaverMapProvider.tsx
+++ src/feature/Provider/NaverMapProvider/NaverMapProvider.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import { ENV } from "@/config";
-import { Container, NavermapsProvider } from "react-naver-maps";
+import { NavermapsProvider } from "react-naver-maps";
 
 export const NaverMapProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <NavermapsProvider ncpClientId={ENV.NAVER_MAP_CLIENT_ID}>
-      <Container className="w-full h-full">{children} </Container>
-    </NavermapsProvider>
-  );
+  return <NavermapsProvider ncpClientId={ENV.NAVER_MAP_CLIENT_ID}>{children}</NavermapsProvider>;
 };
