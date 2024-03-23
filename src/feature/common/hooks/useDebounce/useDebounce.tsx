9289596a6e9fb312dd3acaa453e71fe1b4c39401
ateals <ateals@icloud.com>
2024-03-23 23:28:17 +0900

    feat: useDebounce
    useDebounce 훅

---
+++ src/feature/common/hooks/useDebounce/useDebounce.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useMemo } from "react";
+
+type DebounceCallback<Params> = (...args: Params[]) => void;
+
+export function debounce<Params extends any>(
+  callback: DebounceCallback<Params>,
+  delay: number = 300
+) {
+  let timer: NodeJS.Timeout;
+
+  const debounce = (...args: Params[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+  const clear = () => clearTimeout(timer);
+
+  return { debounce, clear };
+}
+
+export const useDebounce = <Params extends any>(
+  callback: DebounceCallback<Params>,
+  delay: number = 300
+) => {
+  const { debounce: debounceCallback, clear } = useMemo(
+    () => debounce(callback, delay),
+    [callback, delay]
+  );
+
+  useEffect(() => {
+    return () => clear();
+  }, [clear]);
+
+  return debounceCallback;
+};
