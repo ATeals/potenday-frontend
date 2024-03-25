2fe8827600e35ac1eb1952d162bca8e47d9e37f7
ateals <ateals@icloud.com>
2024-03-25 14:39:00 +0900

    feat(UI): Heading

---
+++ src/feature/UI/Heading/Heading.tsx
@@ -0,0 +1,34 @@
+import { generateClassName } from "@/feature/common/utils";
+import { AsChildProps, Slot } from "@UI/Slot";
+import { PolymorphicComponentProps } from "@UI/types/PolymorphicComponentProps";
+
+type HeadingProps = {
+  size?: keyof typeof HeadingSizeMap;
+} & AsChildProps;
+
+const HeadingSizeMap = {
+  sm: "text-xl",
+  md: "text-2xl",
+  lg: "text-3xl",
+  xl: "text-4xl",
+  "2xl": "text-5xl",
+};
+
+export const Heading = <T extends React.ElementType = "h1">({
+  as,
+  size = "md",
+  children,
+  className,
+  asChild,
+  ...props
+}: PolymorphicComponentProps<T, HeadingProps>) => {
+  const Element = asChild ? Slot : as || "h1";
+
+  const tailwind = generateClassName("font-bold", HeadingSizeMap[size], className);
+
+  return (
+    <Element className={tailwind} {...props}>
+      {children}
+    </Element>
+  );
+};
