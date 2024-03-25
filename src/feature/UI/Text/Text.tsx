965ffc17eac591f5ed49d5f778f25b1177c904b4
ateals <ateals@icloud.com>
2024-03-25 14:39:40 +0900

    feat(UI): text

---
+++ src/feature/UI/Text/Text.tsx
@@ -0,0 +1,34 @@
+import { generateClassName } from "@/feature/common/utils";
+import { AsChildProps, Slot } from "@UI/Slot";
+import { PolymorphicComponentProps } from "@UI/types/PolymorphicComponentProps";
+
+type TextProps = {
+  size?: keyof typeof TextSizeMap;
+} & AsChildProps;
+
+const TextSizeMap = {
+  sm: "text-sm",
+  md: "text-md",
+  lg: "text-lg",
+  xl: "text-xl",
+  "2xl": "text-2xl",
+};
+
+export const Text = <T extends React.ElementType = "p">({
+  as,
+  size = "md",
+  children,
+  className,
+  asChild,
+  ...props
+}: PolymorphicComponentProps<T, TextProps>) => {
+  const Element = asChild ? Slot : as || "p";
+
+  const tailwind = generateClassName(TextSizeMap[size], className);
+
+  return (
+    <Element className={tailwind} {...props}>
+      {children}
+    </Element>
+  );
+};
