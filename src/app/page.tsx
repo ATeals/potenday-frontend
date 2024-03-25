3c247f21e0ca46791ca6749dab02f2185c8a4dfa
ateals <ateals@icloud.com>
2024-03-25 16:49:26 +0900

    fix: 목업 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,115 +96,47 @@ const HomePage = () => {
           <Text className="text-gray-400">더보기 &rarr;</Text>
         </Flex>
 
-        <Flex as="section" style={{ direction: "column" }}>
-          <Flex
-            style={{ justify: "space-between" }}
-            className="rounded-lg shadow-lg w-full h-[150px] p-2"
-          >
-            <Box className="w-[25%] bg-gray-300 h-full rounded-lg"></Box>
-            <Flex
-              style={{ direction: "column", align: "start", justify: "start", gap: 5 }}
-              className="h-full w-[75%] p-2"
-            >
-              {/* /뱃지 */}
-              <Flex style={{ gap: 5 }}>
-                <Badge>
-                  <Text size="sm">🔥 실시간 인기 파티</Text>
-                </Badge>
-                <Badge>
-                  <Text size="sm">구내 식당</Text>
-                </Badge>
-              </Flex>
+        {Array(3)
+          .fill(0)
+          .map((_, i) => (
+            <Flex key={i} as="section" style={{ direction: "column" }}>
+              <Flex
+                style={{ justify: "space-between" }}
+                className="rounded-lg shadow-lg w-full h-[150px]  p-2"
+              >
+                <div className="w-[25%] bg-gray-300 h-full rounded-lg" />
+                <Flex
+                  style={{ direction: "column", align: "start", justify: "start", gap: 5 }}
+                  className="h-full w-[75%] p-2"
+                >
+                  {/* /뱃지 */}
+                  <Flex style={{ gap: 5 }}>
+                    <Badge className=" whitespace-nowrap">
+                      <Text size="sm">🔥 실시간 인기 파티</Text>
+                    </Badge>
+                    <Badge className=" whitespace-nowrap">
+                      <Text size="sm">구내 식당</Text>
+                    </Badge>
+                  </Flex>
 
-              <Heading size="sm">자린고비 🐟 점심값 아끼기 도시락 팟 ! 🍱</Heading>
-              <Text size="sm" className="text-gray-500">
-                3월 25일 (월) 13:00
-              </Text>
-              <Text size="sm" className="text-gray-500">
-                📍 3층 4 세미나실
-              </Text>
+                  <Heading size="sm">자린고비 🐟 점심값 아끼기 도시락 팟 ! 🍱</Heading>
+                  <Text size="sm" className="text-gray-500">
+                    3월 25일 (월) 13:00
+                  </Text>
+                  <Text size="sm" className="text-gray-500">
+                    📍 3층 4 세미나실
+                  </Text>
 
-              <Flex>
-                <Box size="sm" className="rounded-[50%] mx-[-5px]"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-primary-lg"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-md"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-black"></Box>
-              </Flex>
-            </Flex>
-          </Flex>
-
-          <Flex
-            style={{ justify: "space-between" }}
-            className="rounded-lg shadow-lg w-full h-[150px] p-2"
-          >
-            <Box className="w-[25%] bg-gray-300 h-full rounded-lg"></Box>
-            <Flex
-              style={{ direction: "column", align: "start", justify: "start", gap: 5 }}
-              className="h-full w-[75%] p-2"
-            >
-              {/* /뱃지 */}
-              <Flex style={{ gap: 5 }}>
-                <Badge>
-                  <Text size="sm">🔥 실시간 인기 파티</Text>
-                </Badge>
-                <Badge>
-                  <Text size="sm">구내 식당</Text>
-                </Badge>
-              </Flex>
-
-              <Heading size="sm">자린고비 🐟 점심값 아끼기 도시락 팟 ! 🍱</Heading>
-              <Text size="sm" className="text-gray-500">
-                3월 25일 (월) 13:00
-              </Text>
-              <Text size="sm" className="text-gray-500">
-                📍 3층 4 세미나실
-              </Text>
-
-              <Flex>
-                <Box size="sm" className="rounded-[50%] mx-[-5px]"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-primary-lg"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-md"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-black"></Box>
+                  <Flex>
+                    <Box size="sm" className="rounded-[50%] mx-[-5px]"></Box>
+                    <Box size="sm" className="rounded-[50%] mx-[-5px] bg-primary-lg"></Box>
+                    <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-md"></Box>
+                    <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-black"></Box>
+                  </Flex>
+                </Flex>
               </Flex>
             </Flex>
-          </Flex>
-
-          <Flex
-            style={{ justify: "space-between" }}
-            className="rounded-lg shadow-lg w-full h-[150px] p-2"
-          >
-            <Box className="w-[25%] bg-gray-300 h-full rounded-lg"></Box>
-            <Flex
-              style={{ direction: "column", align: "start", justify: "start", gap: 5 }}
-              className="h-full w-[75%] p-2"
-            >
-              {/* /뱃지 */}
-              <Flex style={{ gap: 5 }}>
-                <Badge>
-                  <Text size="sm">🔥 실시간 인기 파티</Text>
-                </Badge>
-                <Badge>
-                  <Text size="sm">구내 식당</Text>
-                </Badge>
-              </Flex>
-
-              <Heading size="sm">자린고비 🐟 점심값 아끼기 도시락 팟 ! 🍱</Heading>
-              <Text size="sm" className="text-gray-500">
-                3월 25일 (월) 13:00
-              </Text>
-              <Text size="sm" className="text-gray-500">
-                📍 3층 4 세미나실
-              </Text>
-
-              <Flex>
-                <Box size="sm" className="rounded-[50%] mx-[-5px]"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-primary-lg"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-md"></Box>
-                <Box size="sm" className="rounded-[50%] mx-[-5px] bg-secondary-black"></Box>
-              </Flex>
-            </Flex>
-          </Flex>
-        </Flex>
+          ))}
       </section>
 
       <section className="px-3 py-5">
